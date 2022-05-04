1919923f2d1c46fff5fa19affab528aef5fb33bd
Ulisesmoreno97 <morenofulises@gmail.com>
2022-05-03 22:04:31 -0300

    itemList

--- desafios/src/components/ItemListContainer.jsx
+++ desafios/src/components/ItemListContainer.jsx
@@ -6,11 +6,18 @@ const ItemListContainer = ({
     textoBtn,
     colorBtn,
     parrafo,
-    imagen,
-    buttonStyle,
-    titulo,
-    cantidad
+    imagen,    
+    titulo
+    
 }) => {
+
+  const onAdd = (cantidad) =>{
+    console.log('SELECCIONASTE ${cantidad}')
+  }
+
+
+
+
   return (
       <div className= {'card & ${color}'} style={{ width: "18rem" }}>
         <img src={imagen} className="card-img-top" alt="..." />
@@ -18,8 +25,8 @@ const ItemListContainer = ({
             <h5 className="card-title">{titulo}</h5>
             <p>{greeting}</p>
             <p className="card-text">{parrafo}</p>
-            <ItemCount stock={cantidad}/>
-        <a href="#" className={colorBtn || "btn btn-primary"} >{textoBtn}</a>
+            <ItemCount stock={10} initial={1} onAdd={onAdd}/>
+        {/* <a href="#" className={colorBtn || "btn btn-primary"} >{textoBtn}</a> */}
         </div>
     </div>
   )
