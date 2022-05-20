0b64a0a8b2a09552183bb3690b71d70fa448f0b5
Ulisesmoreno97 <morenofulises@gmail.com>
2022-05-19 21:31:45 -0300

    itemdetail terminado

--- desafios/src/components/Item.jsx
+++ desafios/src/components/Item.jsx
@@ -5,7 +5,7 @@ import ItemCount from './ItemCount'
 const Item = ({items}) => {
 
    const onAdd = (count) => {
-     console.log('SELECCIONASTE ${count}')
+     console.log(`SELECCIONASTE ${count}`)
    }
 
   return (
@@ -19,7 +19,7 @@ const Item = ({items}) => {
        <div className="card-body">
           <h5 className="card-title">{item.title}</h5>
           <p>{item.resumen}</p>
-          <h6>{item.price}</h6>
+          <h6>${item.price}</h6>
           <ItemCount stock={item.stock} initial={1} onAdd={onAdd} />
           <Link to={`/ItemDetailContainer/${item.id}`}  type='button' className='btn btn-primary my-1' > Detalle </Link>         
        </div>
--- desafios/src/components/detalle/ItemDetail.jsx
+++ desafios/src/components/detalle/ItemDetail.jsx
@@ -1,18 +1,53 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ItemCount from '../ItemCount'
 
 const ItemDetail = ({producto}) => {
-console.log(producto)
-  return (
-    <>
-    <div>
-      <h1>{producto.id}</h1>
-      <h2>{producto.resumen}</h2>
-      <h3>{producto.title}</h3>
-      <p>{producto.price}</p>
+  console.log(producto)
 
+  const onAdd = (count) => {
+    console.log(`SELECCIONASTE ${count}`)
+  }
+
+  return (
+    
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
       
+    }}>
+
+      <div className='card text-center' style={{ width: "18rem"}}>
+        <img src={producto.pictureUrl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{producto.title}</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">An item</li>
+              <li className="list-group-item">${producto.price}</li>
+            
+            </ul>
+            <div className="card-body">
+              <ItemCount stock={producto.stock} initial={1} onAdd={onAdd} />
+              <Link to={`/ItemDetailContainer/${producto.id}`} className="card-link">Comprar</Link>
+              <Link to={`/ItemDetailContainer/${producto.id}`} className="card-link">Another link</Link>
+          </div>
+        </div>
     </div>
-    </>
+
+
+
+
+
+
+
+
+
+
+   
+    
   )
 }
 
